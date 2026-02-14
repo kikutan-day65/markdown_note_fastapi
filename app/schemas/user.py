d5669b7590dd4d8c020cdbd7527163c182f8451a
haruto.ubuntu <haruto-cs@outlook.com>
2026-02-14 19:29:40 +0900

    Add user schema

---
+++ app/schemas/user.py
@@ -0,0 +1,56 @@
+import uuid
+from datetime import datetime
+
+from pydantic import BaseModel, EmailStr
+
+
+# ===== REQUEST =====
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    avatar_url: str | None = None
+
+
+class UserUpdate(BaseModel):
+    username: str | None = None
+    avatar_url: str | None = None
+
+
+class UserChangeEmail(BaseModel):
+    email: EmailStr
+
+
+class UserChangePassword(BaseModel):
+    current_password: str
+    new_password: str
+
+
+# ===== RESPONSE =====
+class UserPublic(BaseModel):
+    id: uuid.UUID
+    username: str
+    avatar_url: str | None
+    created_at: datetime
+    updated_at: datetime
+
+
+class UserAdmin(BaseModel):
+    id: uuid.UUID
+    username: str
+    email: EmailStr
+    is_admin: bool
+    is_active: bool
+    is_verified: bool
+    avatar_url: str | None
+    created_at: datetime
+    updated_at: datetime
+
+
+class UserMe(BaseModel):
+    id: uuid.UUID
+    username: str
+    email: EmailStr
+    avatar_url: str | None
+    created_at: datetime
+    updated_at: datetime
