042756b75c4a561508b25ec03f6f8a8cc5869679
haruto.ubuntu <haruto-cs@outlook.com>
2026-02-23 18:36:26 +0900

    Add user save, get_all, get_by_id repository

---
+++ app/repositories/user.py
@@ -0,0 +1,23 @@
+import uuid
+
+from sqlalchemy.orm import Session
+
+from app.db.models.user import User
+
+
+class UserRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def save(self, user: User) -> User:
+        self.db.add(user)
+        self.db.commit()
+        self.db.refresh(user)
+
+        return user
+
+    def get_all(self) -> list[User]:
+        return self.db.query(User).all()
+
+    def get_by_id(self, user_id: uuid.UUID) -> User | None:
+        return self.db.get(User, user_id)
