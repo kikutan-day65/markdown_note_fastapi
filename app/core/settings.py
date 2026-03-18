2f6e3a317f86a938900850d4184d3201be7e0a47
haruto.ubuntu <haruto-cs@outlook.com>
2026-03-18 20:33:34 +0900

    Add secret key, algorithm and access/refresh token expire settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -8,6 +8,10 @@ class Settings(BaseSettings):
     )
 
     DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
 
 settings = Settings()
