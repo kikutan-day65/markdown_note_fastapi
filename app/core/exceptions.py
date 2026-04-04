588fc65d0dae3447181b3e28ef1028f42e47eca1
haruto.ubuntu <haruto-cs@outlook.com>
2026-04-04 12:36:02 +0900

    Add user not found exception

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -33,3 +33,11 @@ class PermissionDeniedException(HTTPException):
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Permission denied",
         )
+
+
+class UserNotFoundException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found.",
+        )
