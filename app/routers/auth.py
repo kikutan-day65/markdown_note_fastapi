2e845b6c365acbc4401514ccf3be4ea7112e238a
haruto.ubuntu <haruto-cs@outlook.com>
2026-04-09 20:53:57 +0900

    Add router for refresh token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,11 +1,18 @@
+import uuid
 from typing import Annotated
 
+import jwt
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
 from sqlalchemy.orm import Session
 
 from app.api.deps import get_db
-from app.core.exceptions import AuthenticationException
+from app.core.exceptions import (
+    AuthenticationException,
+    CredentialException,
+)
+from app.core.settings import settings
 from app.repositories.auth import AuthRepository
 from app.schemas.auth import Token
 from app.services.auth import AuthService
@@ -33,4 +40,54 @@ def login_for_access_token(
 
     return Token(
         access_token=access_token, refresh_token=refresh_token, token_type="bearer"
+    )
+
+
+@router.post("/refresh")
+def refresh_for_access_token(
+    refresh_token: str, db: Session = Depends(get_db)
+) -> Token:
+    repository = AuthRepository(db)
+    service = AuthService(repository)
+
+    try:
+        # Decode refresh token
+        payload = jwt.decode(
+            refresh_token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+
+        # Check 'type: refresh'
+        if payload.get("type") != "refresh":
+            raise CredentialException()
+
+        # Check 'sub: uuid'
+        user_id = payload.get("sub")
+        if not user_id:
+            raise CredentialException()
+
+        user_id = uuid.UUID(user_id)
+
+    except (InvalidTokenError, ValueError):
+        raise CredentialException()
+
+    # Check user_id exists in db
+    user = repository.get_user_by_id(user_id)
+    if not user:
+        raise CredentialException()
+
+    # Revoke old refresh token
+    service.revoke_refresh_token(user.id, refresh_token)
+
+    # Generate access/refresh token
+    data = {"sub": str(user.id)}
+    new_access_token, _ = service.create_token(data=data, token_kind="access")
+    new_refresh_token, expire = service.create_token(data=data, token_kind="refresh")
+
+    # Save new refresh token
+    service.save_refresh_token(user_id=user.id, token=new_refresh_token, expire=expire)
+
+    return Token(
+        access_token=new_access_token,
+        refresh_token=new_refresh_token,
+        token_type="bearer",
     )
