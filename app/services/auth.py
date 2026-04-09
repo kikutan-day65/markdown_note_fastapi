5c0730cfa837fd7fafbe1a463db1cc8a639b9f5d
haruto.ubuntu <haruto-cs@outlook.com>
2026-04-09 20:56:47 +0900

    Add services for refresh token

--- app/services/auth.py
+++ app/services/auth.py
@@ -1,9 +1,12 @@
+import uuid
 from datetime import datetime, timedelta, timezone
 
 import jwt
 
-from app.core.security import DUMMY_HASH, verify_password
+from app.core.exceptions import CredentialException
+from app.core.security import DUMMY_HASH, get_password_hash, verify_password
 from app.core.settings import settings
+from app.db.models.refresh_token import RefreshToken
 from app.db.models.user import User
 from app.repositories.auth import AuthRepository
 
@@ -58,4 +61,37 @@ class AuthService:
             to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
         )
 
-        return encoded_jwt
+        return encoded_jwt, expire
+
+    def save_refresh_token(
+        self, user_id: uuid.UUID, token: str, expire: datetime
+    ) -> None:
+        data = RefreshToken(
+            user_id=user_id, token=get_password_hash(token), expires_at=expire
+        )
+        self.repository.save_refresh_token(data)
+
+    def revoke_refresh_token(self, user_id: uuid.UUID, token: str) -> None:
+        refresh_tokens = self.repository.get_refresh_tokens(user_id=user_id)
+
+        if not refresh_tokens:
+            raise CredentialException()
+
+        target = None
+
+        for rt in refresh_tokens:
+            if verify_password(token, rt.token):
+                target = rt
+                break
+
+        if not target:
+            raise CredentialException()
+
+        now = datetime.now(tz=timezone.utc)
+        if target.expires_at < now:
+            raise CredentialException()
+
+        target.revoked_at = now
+
+        # Update old refresh token -> revoked_at is set
+        self.repository.save_refresh_token(target)
