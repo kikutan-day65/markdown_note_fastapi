dd2920c86fdaec5e63f2c97dfc553f9b748d45db
haruto.ubuntu <haruto-cs@outlook.com>
2026-04-11 12:01:47 +0900

    Add token reuse excepiton

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -41,3 +41,11 @@ class UserNotFoundException(HTTPException):
             status_code=status.HTTP_404_NOT_FOUND,
             detail="User not found.",
         )
+
+
+class TokenReuseException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token reuse detected",
+        )
