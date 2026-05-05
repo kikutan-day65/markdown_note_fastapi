d8975a9d76f927631bedd60294f593a6f5f5d09d
haruto.ubuntu <haruto-cs@outlook.com>
2026-05-05 10:17:41 +0900

    Add unit tests for get_user_by_id()

--- tests/auth/unit/test_repository.py
+++ tests/auth/unit/test_repository.py
@@ -1,3 +1,5 @@
+import uuid
+
 import pytest
 
 
@@ -25,5 +27,32 @@ def test_get_user_by_identifier_returns_none_when_user_found_but_already_deleted
     identifier = deleted_user.username
 
     result = auth_repository.get_user_by_identifier(identifier)
+
+    assert result is None
+
+
+def test_get_user_by_id_success(auth_repository, user):
+    user_id = user.id
+
+    result = auth_repository.get_user_by_id(user_id)
+
+    assert result is not None
+    assert result.id == user_id
+
+
+def test_get_user_by_id_returns_none_when_user_not_found(auth_repository, user):
+    user_id = uuid.uuid4()
+
+    result = auth_repository.get_user_by_id(user_id)
+
+    assert result is None
+
+
+def test_get_user_by_id_returns_none_when_user_found_but_already_deleted(
+    auth_repository, deleted_user
+):
+    user_id = deleted_user.id
+
+    result = auth_repository.get_user_by_id(user_id)
 
     assert result is None
