12927ea900264194a55e2626b7272651813af349
haruto.ubuntu <haruto-cs@outlook.com>
2026-09-14 20:32:40 +0900

    Add unit of work for transaction

---
+++ app/core/unit_of_work.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+
+
+class UnitOfWork:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def commit(self) -> None:
+        self.db.commit()
+
+    def rollback(self) -> None:
+        self.db.rollback()
